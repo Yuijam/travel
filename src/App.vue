556dff0847e1db5498bb3595c62ccf0b3dffacac
yuijam <yuijam@live.com>
2019-12-24 00:46:55 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>app</h1>
+    <router-link to="/home">to home</router-link>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  created () {
+    console.log('hello app')
   }
 }
 </script>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    home page
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    console.log('created home')
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    text-align: center
+  }
+</style>
