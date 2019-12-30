7d33bf1eea53e15c6acc6291383299a7e0eb8ed1
yuijam <yuijam@live.com>
2019-12-30 16:06:50 +0900

    add city list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import '~styles/variable.less';
+
+  .list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    bottom: 0;
+    width: .4rem;
+    .item {
+      color: @bgColor;
+      line-height: .4rem;
+    }
+  }
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="container" ref="wrapper">
+    <div class="content">
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import '~styles/variable.less';
+  .border-topbottom {
+    // 设置border的颜色
+    &:before {
+      border-color: #ccc;
+    }
+    &:after {
+      border-color: #ccc;
+    }
+  }
+  .border-bottom {
+    // 设置border的颜色
+    &:before {
+      border-color: #ccc;
+    }
+  }
+  .container {
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    .title {
+      line-height: .44rem;
+      padding-left: .2rem;
+      background: #eee;
+      color: #666;
+      font-size: .26rem;
+    }
+    .button-list {
+      padding: .1rem .6rem .1rem .1rem;
+      // 触发bfc 不然下面float后就看不到
+      overflow: hidden;
+      .button-wrapper {
+        width: 33.33%;
+        float: left;
+        .button {
+          text-align: center;
+          margin: .1rem;
+          border: .02rem solid #ccc;
+          padding: .1rem 0;
+        }
+      }
+    }
+    .item-list {
+      .item {
+        line-height: .76rem;
+        padding-left: .2rem;
+      }
+    }
+  }
+</style>
